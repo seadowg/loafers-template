8873ebd07410f144c7c1c20b335c71a7cfae0d76
Callum Stott <callum@seadowg.com>
2014-07-02 17:03:27 -0700

    Initial commit :metal:

---
+++ MyApp.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import com.seadowg.loafers.app.*;
+import com.seadowg.loafers.widget.*;
+import com.seadowg.loafers.collection.*;
+
+public class MyApp extends App {
+  public void open() {
+    // You code here...
+  }
+}
